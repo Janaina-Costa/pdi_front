b576aa83fee07c606cfdc86070a44296ff3ebdde
Janaina-Costa <janainacosta.dev@gmail.com>
2024-03-03 21:32:51 -0300

    add validação aos campos dos formulários

---
+++ app/utils/emailValidate.js
@@ -0,0 +1,5 @@
+function emailValidate (email) {
+  var emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+   return emailPattern.test(email); 
+}
+export default emailValidate
---
+++ scriptPages/validateForms.js
@@ -0,0 +1,103 @@
+import emailValidate from "../app/utils/emailValidate.js";
+
+const fields = document.querySelectorAll(".get_fields")
+const defaultErrorMail = document.querySelectorAll('.default_error_mail')
+const defaultErrorPass = document.querySelectorAll('.default_error_pass')
+const defaultErrorName = document.querySelectorAll('.default_error_name')
+
+
+const validateEmailField = () => {
+  
+  fields.forEach(field=>{
+    field.addEventListener('blur', (e)=>{
+      const fieldName = getField(e)
+     
+      if(fieldName === 'email' && e.target.value === ''){
+        defaultErrorMail.forEach(error=>{
+          error.innerHTML = 'Email é obrigatório'
+          error.style.display = 'block'
+        })
+        return
+      }
+       if(fieldName === 'email' && e.target.value.length > 0){
+        const isValidEmail = emailValidate(e.target.value)
+        if(!isValidEmail){
+          console.log('aqui');
+          defaultErrorMail.forEach(error=>{
+            error.innerHTML = 'Email inválido'
+            error.style.display = 'block'
+          })
+          return
+        }
+        defaultErrorMail.forEach(error=> error.style.display = 'none')
+      } 
+    })
+  }) 
+}
+
+
+const validatePasswordField = () => {
+ 
+  fields.forEach(field=>{
+    field.addEventListener('blur', (e)=>{
+      const fieldName = getField(e)
+      if(fieldName === 'password' && e.target.value === ''){
+        defaultErrorPass.forEach(error=>{
+          error.innerHTML = 'Senha é obrigatória'
+          error.style.display = 'block'
+        })
+        return
+      }
+      if(fieldName === 'password' && e.target.value !== ''){
+        const min = 8
+        const max = 12
+        if(e.target.value.length < min || e.target.value.length > max){
+          defaultErrorPass.forEach(error=>{
+            error.innerHTML = `Senha deve ter no mínimo ${min} caracteres e no máximo ${max} caracteres`
+            error.style.display = 'block'
+          })
+          return
+        }
+
+        defaultErrorPass.forEach(error=> error.style.display = 'none')
+      }
+    })
+  })
+}
+
+const validateNameField = () => {
+  fields.forEach(field=>{
+    field.addEventListener('blur', (e)=>{
+      const fieldName = getField(e)
+      if(fieldName === 'name' && e.target.value === ''){
+        defaultErrorName.forEach(error=>{
+          error.innerHTML = 'Nome é obrigatório'
+          error.style.display = 'block'
+        })
+        return
+      }
+      if(fieldName === 'name' && e.target.value !== ''){
+        const min = 3
+    
+        if(e.target.value.length < min || e.target.value.length > max){
+          defaultErrorName.forEach(error=>{
+            error.innerHTML = `Nome deve ter no mínimo ${min} caracteres.`
+            error.style.display = 'block'
+          })
+          return
+        }
+
+        defaultErrorName.forEach(error=> error.style.display = 'none')
+      }
+    })
+  })
+}
+
+const getField = (field) => {
+  return field.target.name
+}
+
+validateNameField()
+validateEmailField()
+validatePasswordField()
+
